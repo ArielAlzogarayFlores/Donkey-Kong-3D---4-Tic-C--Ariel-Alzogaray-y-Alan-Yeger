b86f3c74c23ec89ce7a34968eec227e1783989ec
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2022-06-02 23:32:03 -0300

    Intento1

---
+++ Assets/scripts/ballInstantiate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballInstantiate : MonoBehaviour
+{
+    public GameObject ball1;
+    public GameObject ball2;
+    Vector3 spawn1 = new Vector3 (-21, 8, 13);
+    Vector3 spawn2 = new Vector3(-23, 8, 13);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball1.transform.position = spawn1;
+        ball2.transform.position = spawn2;
+
+        for (int i=0; i<3; i++)
+        {
+            Instantiate(ball1);
+            Instantiate(ball2);
+        }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/collisions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "ball1(Clone)" && col.gameObject.name == "ball2(Clone)")
+        {
+            transform.position = new Vector3(0, 0.5f, 12.7f);
+        }
+    }
+}
